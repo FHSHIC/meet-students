c3b337864c4ee730aa345f2b8a3903ea1abd22db
WhiteChocolate0 <89031999+WhiteChocolate0@users.noreply.github.com>
2022-01-10 14:35:47 +0800

    v1.3 launch

--- fhsh-2.js
+++ fhsh-2.js
@@ -1,17 +1,18 @@
-document.getElementById('myBtn').addEventListener('click', function(e) {
-    var myBtn = e.target;
-    var current = parseInt(myBtn.getAttribute('angle'));
-    current = (current + 180) % 360;
-    myBtn.style.transform = `rotate(${current}deg)`;
-    myBtn.setAttribute('angle', current);
-    document.getElementById('myDropdown').classList.toggle("show");
-})
+// document.getElementById('myBtn').addEventListener('click', function(e) {
+//     var myBtn = e.target;
+//     var current = parseInt(myBtn.getAttribute('angle'));
+//     current = (current + 180) % 360;
+//     myBtn.style.transform = `rotate(${current}deg)`;
+//     myBtn.setAttribute('angle', current);
+//     document.getElementById('myDropdown').classList.toggle("show");
+// });
 
 var jumpout = document.getElementById('jumpout');
-document.getElementById('letter').addEventListener('click', function(){
+var letter = document.getElementById('letter');
+letter.addEventListener('click', function(){
     jumpout.style.display = 'block';
-})
+});
 
-document.getElementById('jumpout').addEventListener('click', function(){
+jumpout.addEventListener('click', function(){
     jumpout.style.display = 'none';
-})
+});
