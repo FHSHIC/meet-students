08571d27179d9845f92ec8ba818108dc96f47cdb
WhiteChocolate0 <89031999+WhiteChocolate0@users.noreply.github.com>
2022-01-12 13:53:14 +0800

    Add files via upload

--- fhsh-2.js
+++ fhsh-2.js
@@ -8,7 +8,8 @@ var liu = document.getElementById('more-liu');
 var liu_jump = document.getElementById('more-liu-jump');
 var talk = document.getElementById('more-talk');
 var talk_jump = document.getElementById('more-talk-jump');
-
+var arrow1 = document.getElementById('arrow-1');
+var club1 = document.getElementById('club-1');
 var arrow2 = document.getElementById('arrow-2');
 var club2 = document.getElementById('club-2');
 var arrow3 = document.getElementById('arrow-3');
@@ -56,6 +57,56 @@ talk_jump.addEventListener('click', function(){
 });
 
 
-document.getElementById('arrow-1').addEventListener('click',function(){
-    document.getElementById('club-1').style.display = 'block';
+arrow1.addEventListener('click',function(){
+    if (club1.style.display == 'none'){
+        club1.style.display = 'block';
+        club2.style.display = 'none';
+        club3.style.display = 'none';
+        arrow2.style.transform = 'rotate(0deg)';
+        arrow3.style.transform = 'rotate(0deg)';
+        arrow1.style.transform = 'rotate(180deg)';
+    }else{
+        club1.style.display = 'none';
+        club2.style.display = 'none';
+        club3.style.display = 'none';
+        arrow1.style.transform = 'rotate(0deg)';
+        arrow2.style.transform = 'rotate(0deg)';
+        arrow3.style.transform = 'rotate(0deg)';
+    }
+})
+
+arrow2.addEventListener('click',function(){
+    if (club2.style.display == 'none'){
+        club2.style.display = 'block';
+        club1.style.display = 'none';
+        club3.style.display = 'none';
+        arrow1.style.transform = 'rotate(0deg)';
+        arrow3.style.transform = 'rotate(0deg)';
+        arrow2.style.transform = 'rotate(180deg)';
+    }else{
+        club1.style.display = 'none';
+        club2.style.display = 'none';
+        club3.style.display = 'none';
+        arrow1.style.transform = 'rotate(0deg)';
+        arrow2.style.transform = 'rotate(0deg)';
+        arrow3.style.transform = 'rotate(0deg)';
+    }
+})
+
+arrow3.addEventListener('click',function(){
+    if (club3.style.display == 'none'){
+        club3.style.display = 'block';
+        club1.style.display = 'none';
+        club2.style.display = 'none';
+        arrow1.style.transform = 'rotate(0deg)';
+        arrow2.style.transform = 'rotate(0deg)';
+        arrow3.style.transform = 'rotate(180deg)';
+    }else{
+        club1.style.display = 'none';
+        club2.style.display = 'none';
+        club3.style.display = 'none';
+        arrow1.style.transform = 'rotate(0deg)';
+        arrow2.style.transform = 'rotate(0deg)';
+        arrow3.style.transform = 'rotate(0deg)';
+    }
 })
